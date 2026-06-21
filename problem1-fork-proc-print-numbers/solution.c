720367751137ccdf22ea26e9c3a6c906a9f941ad
marusya <fgdtjhfgcnjggkidgn@gmail.com>
2026-06-21 17:07:35 +0300

    Update solution.c

--- problem1-fork-proc-print-numbers/solution.c
+++ problem1-fork-proc-print-numbers/solution.c
@@ -10,37 +10,39 @@ int main(int argc, char *argv[]) {
     }
 
     int n = atoi(argv[1]);
-    
     if (n <= 0) {
         fprintf(stderr, "N must be positive\n");
         return 1;
     }
 
+    // Для N=1 просто выводим "1\n"
+    if (n == 1) {
+        printf("1\n");
+        return 0;
+    }
+
     for (int i = 1; i < n; i++) {
         pid_t pid = fork();
-        
         if (pid == -1) {
             perror("fork");
             return 1;
         }
-        
         if (pid == 0) {
-            printf("%d", i + 1);
-            if (i + 1 < n) {
-                printf(" ");
+            // Дочерний процесс
+            if (i + 1 == n) {
+                // Последний процесс выводит число и перевод строки
+                printf("%d\n", i + 1);
             } else {
-                printf("\n");
+                // Остальные процессы выводят число и пробел
+                printf("%d ", i + 1);
             }
             return 0;
         } else {
-            printf("%d", i);
-            if (i < n) {
-                printf(" ");
-            }
+            // Родительский процесс
+            printf("%d ", i);
+            fflush(stdout);
             wait(NULL);
-            if (i > 1) {
-                return 0;
-            }
+            return 0;
         }
     }
     
