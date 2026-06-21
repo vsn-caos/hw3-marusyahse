a27a1328c8c93dd5225c23c863723dbdbe9e3510
marusya <fgdtjhfgcnjggkidgn@gmail.com>
2026-06-21 17:14:06 +0300

    Update solution.c

--- problem1-fork-proc-print-numbers/solution.c
+++ problem1-fork-proc-print-numbers/solution.c
@@ -1,67 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include <sys/wait.h>
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <IP> <port>\n", argv[0]);
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <N>\n", argv[0]);
         return 1;
     }
 
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == -1) {
-        perror("socket");
-        return 1;
+    int n = atoi(argv[1]);
+    if (n <= 0) {
+        return 0;
     }
 
-    struct sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(atoi(argv[2]));
-    if (inet_pton(AF_INET, argv[1], &addr.sin_addr) <= 0) {
-        perror("inet_pton");
-        close(sock);
-        return 1;
-    }
+    for (int i = 1; i <= n; ++i) {
+        if (i == n) {
+            printf("%d\n", i);
+            break;
+        } else {
+            printf("%d ", i);
+            fflush(stdout);
 
-    if (connect(sock, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
-        perror("connect");
-        close(sock);
-        return 1;
-    }
-
-    char buffer[1024];
-    int num;
-    
-    while (scanf("%d", &num) == 1) {
-        snprintf(buffer, sizeof(buffer), "%d\n", num);
-        if (send(sock, buffer, strlen(buffer), 0) == -1) {
-            perror("send");
-            close(sock);
-            return 1;
+            pid_t pid = fork();
+            if (pid < 0) {
+                perror("fork");
+                return 1;
+            } else if (pid > 0) {
+                wait(NULL);
+                break;
+            }
         }
     }
 
-    shutdown(sock, SHUT_WR);
-
-    char response[4096] = {0};
-    ssize_t bytes;
-    
-    while ((bytes = recv(sock, buffer, sizeof(buffer) - 1, 0)) > 0) {
-        buffer[bytes] = '\0';
-        strcat(response, buffer);
-    }
-
-    if (bytes == -1) {
-        perror("recv");
-        close(sock);
-        return 1;
-    }
-
-    printf("%s", response);
-    close(sock);
     return 0;
 }
